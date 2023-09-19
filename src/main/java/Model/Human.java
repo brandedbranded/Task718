f0ecc20496a3199ad55ae6edcdc3c428b6827844
brandedbranded <128071703+brandedbranded@users.noreply.github.com>
2023-09-19 10:54:13 +0300

    1

---
+++ src/main/java/Model/Human.java
@@ -0,0 +1,15 @@
+package Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+
+public class Human {
+    private String firstName;
+    private String lastName;
+}
+
---
+++ src/main/java/Model/Main.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human("Bob","Marley");
+        System.out.println(human);
+    }
+}
